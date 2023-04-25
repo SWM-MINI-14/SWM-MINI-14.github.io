80952e0c6f9e5e1eb7436fb85567273c6cacdc43
Roddyd94 <pky941129@gmail.com>
2023-04-26 03:52:12 +0900

    2nd build

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-
-import Home from './pages/Home.jsx';
-import QuestionDetail from './pages/QuestionDetail.jsx';
-import Login from './pages/Login.jsx';
-import Signup from './pages/Signup.jsx';
-import {
-    BrowserRouter,
-    Route,
-    Routes
-} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import QuestionDetail from "./pages/QuestionDetail.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <Routes>
-                <Route key="/" path="/" exact element={<Home/>} />
-                <Route key="/questions" path="/questions/:id" element={<QuestionDetail/>} />
-                <Route key="/login" path="/login" element={<Login/>} />
-                <Route key="/signup" path="/signup" element={<Signup/>} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route key="/" path="/" exact element={<Home />} />
+        <Route key="/questions" path="/questions/:id" element={<QuestionDetail />} />
+        <Route key="/login" path="/login" element={<Login />} />
+        <Route key="/signup" path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,77 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Container, Typography, List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { identiconSvg } from 'https://cdn.jsdelivr.net/npm/minidenticons@3.1.2/minidenticons.min.js'
-import listJson from '../dummy/list.json';
-import ScrollTrigger from 'react-scroll-trigger';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Container, Typography, List, ListItem, ListItemText, ListItemAvatar, Avatar } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { identiconSvg } from "https://cdn.jsdelivr.net/npm/minidenticons@3.1.2/minidenticons.min.js";
+import listJson from "../dummy/list.json";
+import ScrollTrigger from "react-scroll-trigger";
 
 const useStyles = makeStyles({
-    container: {
-        marginTop: '2rem',
-    },
-    title: {
-        marginBottom: '1rem',
-    },
-    listItem: {
-        marginBottom: '1rem',
-        backgroundColor: '#ffffff',
-        borderRadius: '8px',
-        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
-        opacity: 0,
-        transform: 'translateY(50px)',
-        transition: 'opacity 1s ease, transform 1s ease',
-    },
-    triggered: { // triggered 클래스 정의
-        opacity: 1,
-        transform: 'translateY(0)',
-    },
+  container: {
+    marginTop: "2rem",
+  },
+  title: {
+    marginBottom: "1rem",
+  },
+  listItem: {
+    marginBottom: "1rem",
+    backgroundColor: "#ffffff",
+    borderRadius: "8px",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
+    opacity: 0,
+    transform: "translateY(50px)",
+    transition: "opacity 1s ease, transform 1s ease",
+  },
+  triggered: {
+    // triggered 클래스 정의
+    opacity: 1,
+    transform: "translateY(0)",
+  },
 });
 
 const Home = () => {
-    const [questions, setQuestions] = useState([]);
-    const [triggered, setTriggered] = useState(false);
-    const classes = useStyles();
+  const [questions, setQuestions] = useState([]);
+  const [triggered, setTriggered] = useState(false);
+  const classes = useStyles();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = listJson;
-                setQuestions(response.items);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = listJson;
+        setQuestions(response.items);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    const onEnterViewport = () => {
-        setTriggered(true);
-    }
+  const onEnterViewport = () => {
+    setTriggered(true);
+  };
 
-    return (
-        <Container className={classes.container}>
-            <Typography variant="h4" className={classes.title}>질문 목록</Typography>
-            <List>
-                {questions.map((question, idx) => (
-                    <ScrollTrigger key={question.question_id} onEnter={onEnterViewport}>
-                        <ListItem
-                            className={`${classes.listItem} ${triggered ? classes.triggered : ''}`}
-                            component="a"
-                            href={`/questions/${idx}`}
-                            alignItems="flex-start"
-                        >
-                            <ListItemAvatar style={{marginTop: '0'}}>
-                                <identicon-svg style={{marginRight: '10'}} username={question.owner.display_name}></identicon-svg>
-                            </ListItemAvatar>
-                            <ListItemText primary={question.title} secondary={`작성자: ${question.owner.display_name}`} />
-                        </ListItem>
-                    </ScrollTrigger>
-                ))}
-            </List>
-        </Container>
-    );
+  return (
+    <Container className={classes.container}>
+      <Typography variant="h4" className={classes.title}>
+        질문 목록
+      </Typography>
+      <List>
+        {questions.map((question, idx) => (
+          <ScrollTrigger key={question.question_id} onEnter={onEnterViewport}>
+            <Link to={`/questions/${idx}`}>
+              <ListItem className={`${classes.listItem} ${triggered ? classes.triggered : ""}`} component="a" alignItems="flex-start">
+                <ListItemAvatar style={{ marginTop: "0" }}>
+                  <identicon-svg style={{ marginRight: "10" }} username={question.owner.display_name}></identicon-svg>
+                </ListItemAvatar>
+                <ListItemText primary={question.title} secondary={`작성자: ${question.owner.display_name}`} />
+              </ListItem>
+            </Link>
+          </ScrollTrigger>
+        ))}
+      </List>
+    </Container>
+  );
 };
 
 export default Home;
